56579da614e6e5f0a37e78920572b130d52d6afb
Rose Martus <martus.rose@gmail.com>
2021-03-25 16:22:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,9 +35,8 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age){
     case age > 12 && age < 20:
-    return "You are a teenager"
+      return "You are a teenager"
     default:
-    return "You have an age"
+      return "You have an age"
   }
-
 }
