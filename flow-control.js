763004e0f1012c3d7bff7fa2d61d727fff249eb2
Rose Martus <martus.rose@gmail.com>
2021-03-25 16:16:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,12 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
